fa083d1b2df665c8a13862da0dba73a7038998ca
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-02-17 17:01:07 +0000

    ajouter la logique du composant offer-details

---
+++ src/app/features/offer/offer-details/offer-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-details',
+  imports: [],
+  templateUrl: './offer-details.html',
+  standalone: true,
+})
+export class OfferDetails {
+
+}
